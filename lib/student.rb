03d51b46f32044181ec0f98d31ee60c2a79fa989
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-30 16:03:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,14 +16,21 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash| #each student is a hash
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
-
+    @@all
   end
 end
