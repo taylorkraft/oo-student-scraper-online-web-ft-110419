d2d8da7b6b07dac3a54a87391d379dc6afb721f5
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-30 16:02:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,14 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    def initialize(student_hash)
+    #@name = student_hash[:name]
+    self.send("name=", student_hash[:name])
+    #@location = student_hash[:location]
+    self.send("location=", student_hash[:location])
+    #@profile_url = student_hash[:profile_url]
+    self.send("profile_url=", student_hash[:profile_url])
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
